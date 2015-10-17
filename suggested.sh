cda33ecc1889a784cb0d16fdb94c4d51230f42a7
Franklin Yu <franklinyu@hotmail.com>
2015-10-17 11:24:10 -0400

    add 'irbrc' file

--- suggested.sh
+++ suggested.sh
@@ -21,6 +21,7 @@ brew install gpg
 brew_check $?
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
+echo 'IRB.conf[:PROMPT_MODE] = :SIMPLE' >> ~/.irbrc
 
 # Git
 full_name_default="$(dscl . -read /Users/`whoami` RealName | tr "\n" " " |

44060da59868ca40c34c828507e12320191f986b
Franklin Yu <franklinyu@hotmail.com>
2016-06-30 18:08:13 -0400

    Use long options since available

--- suggested.sh
+++ suggested.sh
@@ -24,7 +24,7 @@ ssh-add ~/.ssh/id_rsa
 # RVM and Ruby
 gpg --keyserver hkp://keys.gnupg.net \
 	--recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
+curl --fail --silent --show-error --location https://get.rvm.io | bash -s stable
 echo 'IRB.conf[:PROMPT_MODE] = :SIMPLE' >> ~/.irbrc
 
 # Git
